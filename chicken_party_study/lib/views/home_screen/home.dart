decb1a718645a64319c8618e786dabed2227b500
JAEEUN SHIN <122805045+JaeeunSkywalker@users.noreply.github.com>
2023-04-03 23:48:19 +0900

    스터디 공개 유무에 따라 화면에 나오고 안 나오는 처리

--- chicken_party_study/lib/views/home_screen/home.dart
+++ chicken_party_study/lib/views/home_screen/home.dart
@@ -41,101 +41,108 @@ class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: appBarWidget(widget.isloggedin),
-        bottomNavigationBar: MainBottomNavigationBar(
-          items: items,
-        ),
-        floatingActionButton: widget.isloggedin == true
-            ? FloatingActionButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return const AlertDialog(
-                        title: Text('스터디 그룹 만들기', textAlign: TextAlign.center),
-                        content: MakeGroupStudy(),
-                      );
-                    },
-                  );
-                },
-                child: const Icon(Icons.add),
-              )
-            : null,
-        body: StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance
-                .collection('studiesOnRecruiting')
-                .snapshots(),
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(
-                    child: CircularProgressIndicator(
-                  color: red,
-                ));
-              }
-              final studios = snapshot.data!.docs;
-              return Container(
-                decoration: BoxDecoration(
-                  image: const DecorationImage(
-                    image: NetworkImage(mainBodyImage),
-                    fit: BoxFit.cover,
-                  ),
-                  border: Border(
-                    bottom: BorderSide(
-                      width: 1,
-                      color: Colors.grey[300]!,
-                    ),
-                  ),
-                ),
-                child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: studios.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final studio = studios[index];
-                    String startdate = (studio['startDate']);
-                    String enddate = (studio['endDate']);
-
-                    return InkWell(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: white,
-                          border: Border(
-                            bottom: BorderSide(
-                              width: 2,
-                              color: Colors.grey[300]!,
-                            ),
-                          ),
-                        ),
-                        child: ListTile(
-                          title: Text(studio['studyGoal']),
-                          subtitle: Text(studio['groupDescription']),
-                          trailing: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text('시작일: $startdate'),
-                              Text('마감일: $enddate'),
-                            ],
-                          ),
-                          onTap: () async {
-                            final docSnapshot = await FirebaseFirestore.instance
-                                .collection('studiesOnRecruiting')
-                                .doc(studio.id)
-                                .get();
-
-                            // 이후 studyData를 study_details_screen으로 전달하여 사용할 수 있습니다.
-                            // ignore: unused_local_variable
-                            final studyData = docSnapshot.data();
-
-                            Get.to(() =>
-                                StudyDetailsScreen(newGroupId: studio.id));
-                          },
-                        ),
-                      ),
+      appBar: appBarWidget(widget.isloggedin),
+      bottomNavigationBar: MainBottomNavigationBar(
+        items: items,
+      ),
+      floatingActionButton: widget.isloggedin == true
+          ? FloatingActionButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const AlertDialog(
+                      title: Text('스터디 그룹 만들기', textAlign: TextAlign.center),
+                      content: MakeGroupStudy(),
                     );
                   },
+                );
+              },
+              child: const Icon(Icons.add),
+            )
+          : null,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('studiesOnRecruiting')
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+                child: CircularProgressIndicator(
+              color: red,
+            ));
+          }
+          final studios = snapshot.data!.docs;
+          return Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: NetworkImage(mainBodyImage),
+                fit: BoxFit.cover,
+              ),
+              border: Border(
+                bottom: BorderSide(
+                  width: 1,
+                  color: Colors.grey[300]!,
                 ),
-              );
-            }));
+              ),
+            ),
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: studios.length,
+              itemBuilder: (BuildContext context, int index) {
+                final studio = studios[index];
+
+                String startdate = (studio['startDate']);
+                String enddate = (studio['endDate']);
+                //스터디 공개 유무에 따라 노출 처리
+                //isPublic이 false인 스터디는 개설자에게만 노출된다.
+                if (studio['isPublic'] == true) {
+                  return InkWell(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: white,
+                        border: Border(
+                          bottom: BorderSide(
+                            width: 2,
+                            color: Colors.grey[300]!,
+                          ),
+                        ),
+                      ),
+                      child: ListTile(
+                        title: Text(studio['studyGoal']),
+                        subtitle: Text(studio['groupDescription']),
+                        trailing: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text('시작일: $startdate'),
+                            Text('마감일: $enddate'),
+                          ],
+                        ),
+                        onTap: () async {
+                          final docSnapshot = await FirebaseFirestore.instance
+                              .collection('studiesOnRecruiting')
+                              .doc(studio.id)
+                              .get();
+
+                          // 이후 studyData를 study_details_screen으로 전달하여 사용할 수 있다.
+                          // ignore: unused_local_variable
+                          final studyData = docSnapshot.data();
+
+                          Get.to(
+                              () => StudyDetailsScreen(newGroupId: studio.id));
+                        },
+                      ),
+                    ),
+                  );
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
 }
